49157e2f895f5caa18a7b9dd74427db1cc1ee7ba
devworker56 <alainkavena@gmail.com>
2025-12-06 13:08:47 -0500

    Changed header.php to match index.php's header

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,8 @@
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top border-bottom border-nasdaq-blue">
     <div class="container">
-        <a class="navbar-brand d-flex align-items-center" href="https://accutradingsignals.com/">
+        <a class="navbar-brand d-flex align-items-center" href="index.php">
+            <img src="assets/images/logo.png" alt="AccuTrading Signals" height="40" class="me-2">
             <span class="fw-bold text-nasdaq-blue">AccuTrading</span>
             <span class="text-light ms-1">Signals</span>
         </a>
@@ -16,25 +17,29 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="https://accutradingsignals.com/">Home</a>
+                    <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="pricing.php">Pricing</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
                 
                 <?php if(isset($_SESSION['user_id'])): ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="dashboard/">Dashboard</a>
-                    </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown">
+                            <i class="bi bi-person-circle me-1"></i>
                             <?php echo htmlspecialchars($_SESSION['user_email']); ?>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="dashboard/">Dashboard</a></li>
-                            <li><a class="dropdown-item" href="dashboard/profile.php">Profile</a></li>
+                            <li><a class="dropdown-item" href="dashboard/"><i class="bi bi-speedometer2 me-2"></i>Dashboard</a></li>
+                            <li><a class="dropdown-item" href="dashboard/profile.php"><i class="bi bi-gear me-2"></i>Settings</a></li>
                             <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item text-danger" href="logout.php">Logout</a></li>
+                            <li><a class="dropdown-item text-danger" href="logout.php"><i class="bi bi-box-arrow-right me-2"></i>Logout</a></li>
                         </ul>
                     </li>
                 <?php else: ?>
@@ -42,7 +47,7 @@
                         <a class="nav-link" href="login.php">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="btn btn-nasdaq-blue ms-2" href="register.php">Free Trial</a>
+                        <a class="btn btn-nasdaq-blue ms-2" href="register.php">Get Started</a>
                     </li>
                 <?php endif; ?>
             </ul>
